3a00941d60de4f485299c7827770137586fd6b42
baurine <2008.hbl@gmail.com>
2018-11-17 18:19:04 +0800

    Create pages dynamically by createPage API

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,4 @@
+const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
@@ -15,4 +16,39 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
       value: slug,
     })
   }
+}
+
+exports.createPages = ({ graphql, actions }) => {
+  // console.log('Create Pages ========>')
+  const { createPage } = actions
+  return new Promise((resolve, reject) => {
+    graphql(`
+      {
+        allMarkdownRemark {
+          edges {
+            node {
+              fields {
+                slug
+              }
+            }
+          }
+        }
+      }
+    `
+).then(result => {
+      // console.log(JSON.stringify(result, null, 4))
+      result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+        createPage({
+          path: node.fields.slug,
+          component: path.resolve(`./src/templates/blog-post.js`),
+          context: {
+            // Data passed to context is available
+            // in page queries as GraphQL variables.
+            slug: node.fields.slug,
+          },
+        })
+      })
+      resolve()
+    })
+  })
 }
---
+++ src/templates/blog-post.js
@@ -0,0 +1,10 @@
+import React from "react"
+import BasicLayout from "../layouts/BasicLayout"
+
+export default () => {
+  return (
+    <BasicLayout>
+      <div>Hello blog post</div>
+    </BasicLayout>
+  )
+}
