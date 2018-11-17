dc16e38b8e14024fb92fe366b1cdaad09655cfb8
baurine <2008.hbl@gmail.com>
2018-11-17 22:04:29 +0800

    Try use gatsby-remark-copy-linked-files instead of gatsby-remark-images to copy images to public for markdown file

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,15 +15,30 @@ module.exports = {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
+          // "gatsby-remark-copy-linked-files",
           {
-            resolve: `gatsby-remark-images`,
+            resolve: "gatsby-remark-copy-linked-files",
             options: {
-              // It's important to specify the maxWidth (in pixels) of
-              // the content container as this plugin uses this as the
-              // base for generating different widths of each image.
-              maxWidth: 590,
-            },
+              // `ignoreFileExtensions` defaults to [`png`, `jpg`, `jpeg`, `bmp`, `tiff`]
+              // as we assume you'll use gatsby-remark-images to handle
+              // images in markdown as it automatically creates responsive
+              // versions of images.
+              //
+              // If you'd like to not use gatsby-remark-images and just copy your
+              // original images to the public directory, set
+              // `ignoreFileExtensions` to an empty array.
+              ignoreFileExtensions: [],
+            }
           },
+          // {
+          //   resolve: `gatsby-remark-images`,
+          //   options: {
+          //     // It's important to specify the maxWidth (in pixels) of
+          //     // the content container as this plugin uses this as the
+          //     // base for generating different widths of each image.
+          //     maxWidth: 590,
+          //   },
+          // },
         ],
       },
     },
