57c5ac6741ce66eb412798003eefb040a7c9bffa
baurine <2008.hbl@gmail.com>
2018-12-09 17:29:49 +0800

    Update note

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,24 +1,19 @@
 const path = require(`path`)
-const { createFilePath } = require(`gatsby-source-filesystem`)
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
-  // console.log('\nonCreateNode: ===> ', node.internal.type)
   const { createNodeField } = actions
   if (node.internal.type === 'MarkdownRemark') {
-    // const fileNode = getNode(node.parent)
-    // console.log(`\n`, fileNode.relativePath)
-    const slug = createFilePath({ node, getNode, basePath: `pages` })
-    // console.log(slug)
+    const parentFileNode = getNode(node.parent)
+    const fileName = parentFileNode.name
     createNodeField({
       node,
       name: `slug`,
-      value: slug,
+      value: fileName,
     })
   }
 }
 
 exports.createPages = ({ graphql, actions }) => {
-  // console.log('Create Pages ========>')
   const { createPage } = actions
   return new Promise((resolve, reject) => {
     graphql(`
--- src/posts/test-files/test-files.md
+++ src/posts/test-files/test-files.md
@@ -3,8 +3,8 @@ title: "Test link image and files"
 date: "2017-08-22"
 ---
 
-![](dog-2.jpeg)
+![](./dog-2.jpeg)
 
-[Download PDF for test](carbon.pdf)
+[Download PDF for test](./carbon.pdf)
 
-[Download XLSX for test](file-types-icons.xlsx)
+[Download XLSX for test](./file-types-icons.xlsx)
